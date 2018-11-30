cd819e568c75a3fa01ec0be23f73877e6a1e609c
liwenming <yingmuhuadao@baofu.com>
2018-11-30 14:21:36 +0800

    C初版

---
+++ design-mode-api/src/main/java/com/wmli/system/api/DesignModeApplication.java
@@ -0,0 +1,17 @@
+package com.wmli.system.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author: yingmuhuadao
+ * @date: Created in 10:58 2018/11/30
+ */
+@SpringBootApplication
+public class DesignModeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModeApplication.class,args);
+    }
+}
